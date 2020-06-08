87001bb7b489f0c9ca155a6428108c67e4e2869e
John Conde <stymiee@gmail.com>
2020-06-08 12:38:31 -0400

    First pass at a password strength checker

---
+++ src/PasswordHelper/StrengthChecker.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PasswordHelper;
+
+class StrengthChecker
+{
+    /**
+     * @var int Password strength score
+     */
+    protected $strength;
+
+    protected $levels = [
+        'Very Weak', 'Weak', 'Good', 'Very Good', 'Strong', 'Very Strong'
+    ];
+
+    public function __construct()
+    {
+        $this->strength = 0;
+    }
+
+    public function checkStrength(string $password): string
+    {
+        $score = 0;
+        $score += min([strlen($password), Policy::MINIMUM_LENGTH]);
+        $score += (int) (bool) preg_match_all('/\d/', $password, $matches);
+        $score += (int) (bool) preg_match_all('/[a-z]/i', $password, $matches);
+        $score += (int) (bool) preg_match_all('/[A-Z]/', $password, $matches);
+        $score += (int) (bool) preg_match_all('/[a-z]/', $password, $matches);
+        $score += (int) (bool) preg_match_all('/[^a-z\d ]/i', $password, $matches);
+        $score = min([(int) ($score / 3), 6]);
+
+        return $this->levels[$score];
+    }
+}
